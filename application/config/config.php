73717fe9151021a6e5e79bb67120487380e031ea
Maxwell kemboi <43989639+maxisz@users.noreply.github.com>
2021-07-24 20:28:22 +0300

    update conf.php

--- application/config/config.php
+++ application/config/config.php
@@ -1,8 +1,7 @@
 <?php
-defined('https://mic-study.herokuapp.com/');
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
-OR exit('No direct script access allowed');
 |--------------------------------------------------------------------------
 | Base Site URL
 |--------------------------------------------------------------------------
@@ -25,9 +24,9 @@ OR exit('No direct script access allowed');
 |
 */
 
-$config['https://mic-study.herokuapp.com/'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
-$config['https://mic-study.herokuapp.com/'] .= "://".$_SERVER['HTTP_HOST'];
-$config['https://mic-study.herokuapp.com/'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
+$config['base_url'] .= "https://mic-study.herokuapp.com/";
 
 /*
 |--------------------------------------------------------------------------
