e9d307dc64cc75b0029c3babfa8cda8175c8f4f2
Maxwell kemboi <43989639+maxisz@users.noreply.github.com>
2021-07-24 18:51:41 +0300

    mm

--- application/config/config.php
+++ application/config/config.php
@@ -2,6 +2,7 @@
 defined('https://mic-study.herokuapp.com/');
 
 /*
+OR exit('No direct script access allowed');
 |--------------------------------------------------------------------------
 | Base Site URL
 |--------------------------------------------------------------------------
@@ -24,9 +25,9 @@ defined('https://mic-study.herokuapp.com/');
 |
 */
 
-$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
-$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
-$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['https://mic-study.herokuapp.com/'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['https://mic-study.herokuapp.com/'] .= "://".$_SERVER['HTTP_HOST'];
+$config['https://mic-study.herokuapp.com/'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 
 /*
 |--------------------------------------------------------------------------
